741d2a455f4a88e042b54827ab064be5b2f38c05
iryna1805 <ira.bastriga18@gmail.com>
2025-04-19 23:46:25 +0200

    first commit

---
+++ Market/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Person(models.Model):
+    name = models.CharField(max_length=30)
+    surname = models.CharField(max_length=30)
+    birthday = models.DateField()
+    age = models.IntegerField()
+    mail = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name} {self.surname}"
