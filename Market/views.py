917f9661db08fdd309093552dd90304521f34aa7
iryna1805 <ira.bastriga18@gmail.com>
2025-05-31 18:32:42 +0200

    pagination

--- Market/views.py
+++ Market/views.py
@@ -2,10 +2,23 @@ from django.shortcuts import render, redirect
 from .models import Musician, Album
 from django.contrib.auth import login, authenticate
 from .forms import UserRegisterForm, AdminRegisterForm
+from django.core.paginator import Paginator
+
 
 def home(request):
-    musicians = Musician.objects.all()
-    albums = Album.objects.all()
+    musicians_list = Musician.objects.all()
+    albums_list = Album.objects.all()
+
+    # Пагінація
+    paginator_musicians = Paginator(musicians_list, 5)  
+    page_number_musicians = request.GET.get('page_musicians')
+    musicians = paginator_musicians.get_page(page_number_musicians)
+
+    # Пагінація
+    paginator_albums = Paginator(albums_list, 5)
+    page_number_albums = request.GET.get('page_albums')
+    albums = paginator_albums.get_page(page_number_albums)
+
     return render(request, 'Market/index.html', {'musicians': musicians, 'albums': albums})
 
 def about(request):
